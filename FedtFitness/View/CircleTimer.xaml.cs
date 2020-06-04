960f247cd87e75f67ab115c41f789afd68ffb2f7
Joshua James Lewis <josh1205_1933@yahoo.com>
2020-06-04 16:06:06 +0200

    Stopwatch added

---
+++ FedtFitness/View/CircleTimer.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+
+namespace FedtFitness.View
+{
+    public sealed partial class CircleTimer : UserControl
+    {
+        private double ProgressValue = 0;
+
+        public CircleTimer()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+        }
+        #region Size Property
+        public double Size
+        {
+            get { return (double)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+        private static void OnSizePropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            CircleTimer TheControl = (source as CircleTimer);
+            if (TheControl != null)
+                TheControl.UpdateSizing();
+        }
+        public static readonly DependencyProperty SizeProperty =
+         DependencyProperty.Register("Size", typeof(double), typeof(CircleTimer), new PropertyMetadata((double)28.0, OnSizePropertyChanged));
+
+        private double InsideSize { get { return Size - LineWidth; } }
+        private double HalfInsideSize { get { return (Size - LineWidth) / 2; } }
+        #endregion Size Property
+
+        #region LineWidth
+        public double LineWidth
+        {
+            get { return (double)GetValue(LineWidthProperty); }
+            set { SetValue(LineWidthProperty, value); }
+        }
+        public static readonly DependencyProperty LineWidthProperty =
+            DependencyProperty.Register("LineWidth", typeof(double), typeof(CircleTimer), new PropertyMetadata((double)4.0, OnLineWidthPropertyChanged));
+
+        private static void OnLineWidthPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            CircleTimer TheControl = (source as CircleTimer);
+            if (TheControl != null)
+                TheControl.UpdateSizing();
+        }
+        #endregion LineWidth
+        #region Color
+        public Brush Color
+        {
+            get { return (Brush)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("LineWidth", typeof(Brush), typeof(CircleTimer), new PropertyMetadata(new SolidColorBrush(Colors.White), OnColorPropertyChanged));
+
+        private static void OnColorPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs e)
+        {
+            CircleTimer TheControl = (source as CircleTimer);
+            if (TheControl != null)
+                TheControl.UpdateSizing();
+        }
+        #endregion Color
+
+        private void UpdateSizing()
+        {
+            ThePath.Stroke = Color;
+            ThePath.StrokeThickness = LineWidth;
+            TheGrid.Width = Size;
+            TheGrid.Height = Size;
+            ThePathFigure.StartPoint = new Point(HalfInsideSize, InsideSize);
+            TheSegment.Size = new Windows.Foundation.Size(HalfInsideSize, HalfInsideSize);
+            SetBarLength(ProgressValue);
+        }
+        private void SetBarLengthUI(double Value)
+        {
+            Value = Math.Max(0, Math.Min(1.0, Value));
+            ProgressValue = Value;
+
+            double Angle = 2 * 3.14159265 * ProgressValue;
+
+            double X = HalfInsideSize - Math.Sin(Angle) * HalfInsideSize;
+            double Y = HalfInsideSize + Math.Cos(Angle) * HalfInsideSize;
+
+            if (Value > 0 && (int)X == HalfInsideSize && (int)Y == InsideSize)
+                X += 0.01;
+
+            TheSegment.IsLargeArc = Angle >= 3.14159265;
+            TheSegment.Point = new Point(X, Y);
+        }
+
+        public void SetBarLength(double Value)
+        {
+            if (DesignMode.DesignModeEnabled)
+                SetBarLengthUI(Value);
+            else if (CoreApplication.MainView.CoreWindow.Dispatcher.HasThreadAccess)
+                SetBarLengthUI(Value);
+            else
+            {
+                double LocalValue = Value;
+                IAsyncAction IgnoreMe = CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { SetBarLengthUI(Value); });
+            }
+        }
+    }
+}
--- FedtFitness/View/CustomWorkout.xaml.cs
+++ FedtFitness/View/CustomWorkout.xaml.cs
@@ -12,8 +12,29 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FedtFitness.View;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+//// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+//namespace FedtFitness.View
+//{
+//    /// <summary>
+//    /// An empty page that can be used on its own or navigated to within a Frame.
+//    /// </summary>
+//    public sealed partial class CustomWorkout : Page
+//    {
+//        public CustomWorkout()
+//        {
+//            this.InitializeComponent();
+//        }
+//    }
+//}
+//using System;
+//using Windows.UI.Xaml;
+//using Windows.UI.Xaml.Controls;
+//using Windows.UI.Xaml.Navigation;
+
+//// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace FedtFitness.View
 {
@@ -22,9 +43,115 @@ namespace FedtFitness.View
     /// </summary>
     public sealed partial class CustomWorkout : Page
     {
+        private DispatcherTimer dispatcherTimer, timeDispatcher, demoDispatcher;
+        private int timesTicked = 1;
+        private double ProgressAmount = 0;
+        private int sec = 0;
+        private int min = 0;
+        private int hour = 0;
+
+        private DateTime startedTime;
+        private TimeSpan timePassed, timeSinceLastStop;
+
+
         public CustomWorkout()
         {
             this.InitializeComponent();
+        }
+
+        bool isStop = false;
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (isStop == false)
+            {
+                isStop = true;
+                startedTime = DateTime.Now;
+                DispatcherTimerSetup();
+
+                Start.Content = "Stop";
+            }
+            else
+            {
+                isStop = false;
+                dispatcherTimer.Stop();
+                demoDispatcher.Stop();
+                Hour.Text = "00:00:00:00";
+                Start.Content = "Start";
+                ProgressControl.SetBarLength(0.0);
+                ProgressAmount = 0.0;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            ProgressControl.SetBarLength(1.0);
+        }
+        private void DispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 10);
+            dispatcherTimer.Start();
+
+
+
+            timeSinceLastStop = TimeSpan.Zero;
+            Hour.Text = "00:00:00:00";
+            demoDispatcher = new DispatcherTimer();
+            demoDispatcher.Tick += DemoDispatcher_Tick;
+            demoDispatcher.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            demoDispatcher.Start();
+        }
+        int lapCount = 0;
+        private void Lap_Click(object sender, RoutedEventArgs e)
+        {
+            lapCount++;
+            txtLap.Text += "Lap " + lapCount + ": " + Hour.Text + "\n";
+        }
+
+        private string MakeDigitString(int number, int count)
+        {
+            string result = "0";
+            if (count == 2)
+            {
+                if (number < 10)
+                    result = "0" + number;
+                else
+                    result = number.ToString();
+            }
+            else if (count == 3)
+            {
+                if (number < 10)
+                    result = "00" + number;
+                else if (number > 9 && number < 100)
+                {
+                    result = "0" + number;
+                }
+                else
+                    result = number.ToString();
+            }
+            return result;
+        }
+
+        private void DemoDispatcher_Tick(object sender, object e)
+        {
+            timePassed = DateTime.Now - startedTime;
+            Hour.Text = MakeDigitString((timeSinceLastStop + timePassed).Hours, 2) + ":"
+                + MakeDigitString((timeSinceLastStop + timePassed).Minutes, 2) + ":"
+                + MakeDigitString((timeSinceLastStop + timePassed).Seconds, 2) + ":"
+                + MakeDigitString((timeSinceLastStop + timePassed).Milliseconds, 3);
+        }
+
+
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            timesTicked++;
+            ProgressControl.SetBarLength(ProgressAmount);
+            ProgressAmount += (1.0 / 60.0) * (7.95 / 60.0);
+            if (ProgressAmount > 1.0)
+                ProgressAmount = 0.0;
         }
     }
 }
